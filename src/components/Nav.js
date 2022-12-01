a2cecb1884531bfe8935d44f2bfbe197232e369b
Felix-fale <falefelix23@gmail.com>
2022-12-01 21:07:14 +0000

    first commit

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Nav.scss"
+
+function Nav() {
+    return (
+        <div className='nav'>
+            <button className='nav__burger'></button>
+        </div>
+    );
+}
+
+export default Nav;
