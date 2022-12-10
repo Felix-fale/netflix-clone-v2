957355a29045b7f09752b532d99f2b9c4fbda0b2
Felix-fale <falefelix23@gmail.com>
2022-12-10 14:16:35 +0000

    replace the video to play at the hero(header) level

--- src/components/Video.js
+++ src/components/Video.js
@@ -4,25 +4,17 @@ import "./Video.scss";
 
 function Video(props) {
   //   let { id } = useParams();
-  let id = "dNNH5k1qCR0";
+  let id = "6CHs4x2uqcQ";
   console.log(id);
   return (
     <div className="video">
       <iframe
         src={`https://www.youtube.com/embed/${id}`}
-        title="video"
+        title="Kanye West - Good Morning"
         frameborder="0"
         allowfullscreen
       ></iframe>
-      {/* <iframe
-        width="751"
-        height="563"
-        src="https://www.youtube.com/embed/dNNH5k1qCR0?list=PLYgAq_prE4ql2Xhq3Uln0qfOY38sGymoR"
-        title="Praye - Angelina"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe> */}
+      {/* <iframe width="1027" height="578" src="https://www.youtube.com/embed/6CHs4x2uqcQ?list=PLYgAq_prE4ql2Xhq3Uln0qfOY38sGymoR" title="Kanye West - Good Morning" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
     </div>
   );
 }
